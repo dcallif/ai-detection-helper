e8d8dfa7611bdd02952c8fe1e4f3c9fc812c4331
Daniel Callif <dcallif22@gmail.com>
2025-05-12 06:08:42 -0600

    Complete dark mode implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,20 @@ import { Layout } from './components/Layout';
 import { DetectorView } from './views/DetectorView';
 import { AboutView } from './views/AboutView';
 import { HistoryView } from './views/HistoryView';
-import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   const [currentView, setCurrentView] = React.useState<'detector' | 'about' | 'history'>('detector');
 
   return (
-    <ThemeProvider>
-      <Layout onNavigate={setCurrentView} currentView={currentView}>
-        {currentView === 'detector' ? (
-          <DetectorView />
-        ) : currentView === 'about' ? (
-          <AboutView />
-        ) : (
-          <HistoryView />
-        )}
-      </Layout>
-    </ThemeProvider>
+    <Layout onNavigate={setCurrentView} currentView={currentView}>
+      {currentView === 'detector' ? (
+        <DetectorView />
+      ) : currentView === 'about' ? (
+        <AboutView />
+      ) : (
+        <HistoryView />
+      )}
+    </Layout>
   );
 }
 
