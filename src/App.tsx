e2eefa939a4c9916f388022574227cda177fde89
Daniel Callif <dcallif22@gmail.com>
2025-05-12 06:01:23 -0600

    Implement dark mode with system preference detection

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import { Layout } from './components/Layout';
 import { DetectorView } from './views/DetectorView';
 import { AboutView } from './views/AboutView';
 import { HistoryView } from './views/HistoryView';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   const [currentView, setCurrentView] = React.useState<'detector' | 'about' | 'history'>('detector');
 
   return (
-    <Layout onNavigate={setCurrentView} currentView={currentView}>
-      {currentView === 'detector' ? (
-        <DetectorView />
-      ) : currentView === 'about' ? (
-        <AboutView />
-      ) : (
-        <HistoryView />
-      )}
-    </Layout>
+    <ThemeProvider>
+      <Layout onNavigate={setCurrentView} currentView={currentView}>
+        {currentView === 'detector' ? (
+          <DetectorView />
+        ) : currentView === 'about' ? (
+          <AboutView />
+        ) : (
+          <HistoryView />
+        )}
+      </Layout>
+    </ThemeProvider>
   );
 }
 
